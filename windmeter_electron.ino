9b8da21952f19198a5dd7c446dac832d73530ae8
Peter van Dijk <39601927+peterDijk@users.noreply.github.com>
2019-03-28 13:22:06 +0100

    Update windmeter_electron.ino

--- windmeter_electron.ino
+++ windmeter_electron.ino
@@ -7,7 +7,7 @@ float sensorVoltage = 0; //Variable that stores the voltage (in Volts) from the 
 double windSpeed = 0; // Wind speed in meters per second (m/s)
  
 float voltageConversionConstant = .000805860805861; //This constant maps the value provided from the analog read function, which ranges from 0 to 4095, to actual voltage, which ranges from 0V to 3.3V
-int sensorDelay = 10000; //Delay between sensor readings, measured in milliseconds (ms)
+int sensorDelay = 1000; //Delay between sensor readings, measured in milliseconds (ms)
  
 //Anemometer Technical Variables
 //The following variables correspond to the anemometer sold by Adafruit, but could be modified to fit other anemometers.
@@ -16,7 +16,7 @@ float voltageMin = .4; // Mininum output voltage from anemometer in mV.
 float windSpeedMin = 0; // Wind speed in meters/sec corresponding to minimum voltage
  
 float voltageMax = 2.0; // Maximum output voltage from anemometer in mV.
-float windSpeedMax = 32; // Wind speed in meters/sec corresponding to maximum voltage
+float windSpeedMax = 32.4; // Wind speed in meters/sec corresponding to maximum voltage
  
  
  
@@ -36,7 +36,7 @@ sensorVoltage = sensorValue * voltageConversionConstant; //Convert sensor value 
 if (sensorVoltage <= voltageMin){
  windSpeed = 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
 } else {
-  windSpeed = (sensorVoltage - voltageMin)*windSpeedMax/(voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
+  windSpeed = (sensorVoltage - voltageMin) / (voltageMax - voltageMin) * windSpeedMax; //For voltages above minimum value, use the linear relationship to calculate wind speed.
   Particle.publish("e_s", String(windSpeed, 1), 60, PRIVATE);
 }
  
